ed89ad3ba8106d8e5a9c0a79ef54768cf8f867d6
Elfar Didier Morantes Sanchez <emorantes@cundinamarca.gov.co>
2023-06-02 11:47:05 -0500

    correcion errores de salida

--- application/controllers/powerbi/BO_ENO_controller.php
+++ application/controllers/powerbi/BO_ENO_controller.php
@@ -14,13 +14,21 @@ class BO_ENO_controller extends REST_Controller {
     public $auth_override;
     public $BO_ENO_model;
     // para suprimir errores de php
-    
+
     function __construct() {
         // Construct the parent class
         parent::__construct();
         $this->load->model('powerbi/BO_ENO_model');
     }
 
+
+    public function index_get(){
+        $this->response([
+            'STATUS' => FALSE,
+            'MESSAGE' => 'Proporcione un endpoint completo '
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+    }
+    
 
 
     public function id_evento_padre_get() {
@@ -72,39 +80,52 @@ class BO_ENO_controller extends REST_Controller {
         // quitamos los espacios en blanco codificados en la url como %20
         // $fecha_inic_con_espacio = urldecode($id_evento_padre);
         // limpiamos espacios al principio y al final
-        $fecha_inic_limpio = trim($fecha_inic);
+
       
 
-        if ($fecha_inic_limpio === null) {
+        if ($fecha_inic === null) {
             $this->response([
                 'STATUS' => FALSE,
                 'MESSAGE' => 'Error en el URL'
                     ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
         }
 
-        if ($fecha_inic_limpio === "") {
+        if ($fecha_inic === "") {
             $this->response([
                 'STATUS' => FALSE,
                 'MESSAGE' => 'fecha_inic no ingresado'
                     ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
         } else {
+                    // si hay fecha valida la limpiamos y consultamos. Hacemos segunda validacion porque a veces no toma bien la primera validacion
+                    if($fecha_inic != '' || $fecha_inic != null ){
+                        $fecha_inic_limpio = trim($fecha_inic);
+
+                        $dat = $this->BO_ENO_model->getDataBO_ENOfechaInic($fecha_inic_limpio);
+
+                        if ($dat) {
+                            $this->response([
+                                'STATUS' => TRUE,
+                                'MESSAGE' => 'datos encontrados',
+                                "DATA" => $dat
+                                    ], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                        } else {
+                            $this->response([
+                                'STATUS' => TRUE,
+                                'MESSAGE' => 'datos no encontrados por fecha_inic',
+                                'fecha_inic' => $fecha_inic_limpio
+                                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                        }
 
 
-                    $dat = $this->BO_ENO_model->getDataBO_ENOfechaInic($fecha_inic_limpio);
-
-                    if ($dat) {
-                        $this->response([
-                            'STATUS' => TRUE,
-                            'MESSAGE' => 'datos encontrados',
-                            "DATA" => $dat
-                                ], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-                    } else {
-                        $this->response([
-                            'STATUS' => TRUE,
-                            'MESSAGE' => 'datos no encontrados por fecha_inic',
-                            'id_evento_padre' => $fecha_inic_limpio
-                                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
                     }
+                    else{
+                        $this->response([
+                            'STATUS' => FALSE,
+                            'MESSAGE' => 'fecha_inic no ingresado'
+                                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code 
+                    }
+
+
 
     }
 
